f64cf1f786eac1eb4964f4cf7a51e0d55de57318
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-01-29 22:38:28 +0700

    Move onEvent and disable some feature

--- Module-Lora/src/main.cpp
+++ Module-Lora/src/main.cpp
@@ -1,6 +1,9 @@
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
+#include <Arduino.h>
+
+#include "onEvent.h"
 
 static const PROGMEM u1_t NWKSKEY[16] = {0x28, 0xAE, 0xD2, 0x2B, 0x7E, 0x15, 0x16, 0xA6, 0x09, 0xCF, 0xAB, 0xF7, 0x15, 0x88, 0x4F, 0x3C};
 static const u1_t PROGMEM APPSKEY[16] = {0x16, 0x28, 0xAE, 0x2B, 0x7E, 0x15, 0xD2, 0xA6, 0xAB, 0xF7, 0xCF, 0x4F, 0x3C, 0x15, 0x88, 0x09};
@@ -21,90 +24,9 @@ const lmic_pinmap lmic_pins = {
     .rxtx = LMIC_UNUSED_PIN,
     .rst = 9, // reset pin
     .dio = {2, 3, 4},
+    .rxtx_rx_active = 0,
+    .rssi_cal = 10,
 };
-
-void onEvent(ev_t ev)
-{
-  Serial.print(os_getTime());
-  Serial.print(": ");
-  switch (ev)
-  {
-  case EV_SCAN_TIMEOUT:
-    Serial.println(F("EV_SCAN_TIMEOUT"));
-    break;
-  case EV_BEACON_FOUND:
-    Serial.println(F("EV_BEACON_FOUND"));
-    break;
-  case EV_BEACON_MISSED:
-    Serial.println(F("EV_BEACON_MISSED"));
-    break;
-  case EV_BEACON_TRACKED:
-    Serial.println(F("EV_BEACON_TRACKED"));
-    break;
-  case EV_JOINING:
-    Serial.println(F("EV_JOINING"));
-    break;
-  case EV_JOINED:
-    Serial.println(F("EV_JOINED"));
-    break;
-  /*
-  || This event is defined but not used in the code. No
-  || point in wasting codespace on it.
-  ||
-  || case EV_RFU1:
-  ||     Serial.println(F("EV_RFU1"));
-  ||     break;
-  */
-  case EV_JOIN_FAILED:
-    Serial.println(F("EV_JOIN_FAILED"));
-    break;
-  case EV_REJOIN_FAILED:
-    Serial.println(F("EV_REJOIN_FAILED"));
-    break;
-  case EV_TXCOMPLETE:
-    Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-    if (LMIC.txrxFlags & TXRX_ACK)
-      Serial.println(F("Received ack"));
-    if (LMIC.dataLen)
-    {
-      Serial.println(F("Received "));
-      Serial.println(LMIC.dataLen);
-      Serial.println(F(" bytes of payload"));
-    }
-    break;
-  case EV_LOST_TSYNC:
-    Serial.println(F("EV_LOST_TSYNC"));
-    break;
-  case EV_RESET:
-    Serial.println(F("EV_RESET"));
-    break;
-  case EV_RXCOMPLETE:
-    // data received in ping slot
-    Serial.println(F("EV_RXCOMPLETE"));
-    break;
-  case EV_LINK_DEAD:
-    Serial.println(F("EV_LINK_DEAD"));
-    break;
-  case EV_LINK_ALIVE:
-    Serial.println(F("EV_LINK_ALIVE"));
-    break;
-  /*
-  || This event is defined but not used in the code. No
-  || point in wasting codespace on it.
-  ||
-  || case EV_SCAN_FOUND:
-  ||    Serial.println(F("EV_SCAN_FOUND"));
-  ||    break;
-  */
-  case EV_TXSTART:
-    Serial.println(F("EV_TXSTART"));
-    break;
-  default:
-    Serial.print(F("Unknown event: "));
-    Serial.println((unsigned)ev);
-    break;
-  }
-}
 
 void do_send(osjob_t *j)
 {
