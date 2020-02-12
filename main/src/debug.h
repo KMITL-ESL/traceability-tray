c61467b970f09c35243b11257cbc8a2ecb0ce3d0
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-02-12 17:22:47 +0700

    Add debug function

---
+++ main/src/debug.h
@@ -0,0 +1,32 @@
+#ifndef DEBUG_h
+#define DEBUG_h
+
+#ifdef SHOW_DEBUG_CMD
+#define DEBUG_CMD(msg)                \
+    do                                \
+    {                                 \
+        Serial.print("DEBUG CMD : "); \
+        Serial.println(msg);          \
+    } while (0)
+#else
+#define DEBUG_CMD(msg) \
+    do                 \
+    {                  \
+    } while (0)
+#endif
+
+#ifdef SHOW_DEBUG_I2C
+#define DEBUG_I2C(msg)                \
+    do                                \
+    {                                 \
+        Serial.print("DEBUG I2C : "); \
+        Serial.println(msg);          \
+    } while (0)
+#else
+#define DEBUG_I2C(msg) \
+    do                 \
+    {                  \
+    } while (0)
+#endif
+
+#endif
